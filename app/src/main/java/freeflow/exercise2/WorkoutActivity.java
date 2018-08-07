478ae043480c31b5fa000341e2dd2f535b244406
M C <unsuspiciousguy@gmail.com>
2018-08-07 13:47:28 -0500

    Initial commit

---
+++ app/src/main/java/freeflow/exercise2/WorkoutActivity.java
@@ -0,0 +1,13 @@
+package freeflow.exercise2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout);
+    }
+}
